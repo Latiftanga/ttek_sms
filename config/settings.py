fed0f3a7587114e5778af688c4b4d9a22155ee64
Latif Adams <latiftanga@gmail.com>
2026-01-19 18:11:29 +0000

    Customise Django Admin panel

--- config/settings.py
+++ config/settings.py
@@ -69,6 +69,7 @@ if not DEBUG:
 # --- 2. APPS ---
 SHARED_APPS = (
     'django_tenants',
+    'unfold',  # Django Unfold admin theme - must be before django.contrib.admin
     'schools',
     'accounts',
     'django.contrib.admin',
@@ -444,3 +445,77 @@ USE_TZ = True
 
 # --- 13. DEFAULT PRIMARY KEY ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --- 14. UNFOLD ADMIN THEME ---
+UNFOLD = {
+    "SITE_TITLE": "TTEK SMS",
+    "SITE_HEADER": "TTEK SMS Platform",
+    "SITE_SYMBOL": "school",
+    "SHOW_HISTORY": True,
+    "SHOW_VIEW_ON_SITE": True,
+    "ENVIRONMENT": "core.utils.environment_callback",
+    "COLORS": {
+        "primary": {
+            "50": "250 245 255",
+            "100": "243 232 255",
+            "200": "233 213 255",
+            "300": "216 180 254",
+            "400": "192 132 252",
+            "500": "168 85 247",
+            "600": "147 51 234",
+            "700": "126 34 206",
+            "800": "107 33 168",
+            "900": "88 28 135",
+            "950": "59 7 100",
+        },
+    },
+    "SIDEBAR": {
+        "show_search": True,
+        "show_all_applications": True,
+        "navigation": [
+            {
+                "title": "Schools",
+                "separator": True,
+                "items": [
+                    {
+                        "title": "Schools",
+                        "icon": "school",
+                        "link": "/admin/schools/school/",
+                    },
+                    {
+                        "title": "Regions",
+                        "icon": "map",
+                        "link": "/admin/schools/region/",
+                    },
+                    {
+                        "title": "Districts",
+                        "icon": "location_city",
+                        "link": "/admin/schools/district/",
+                    },
+                ],
+            },
+            {
+                "title": "Users & Auth",
+                "separator": True,
+                "items": [
+                    {
+                        "title": "Users",
+                        "icon": "people",
+                        "link": "/admin/accounts/customuser/",
+                    },
+                ],
+            },
+            {
+                "title": "Tasks",
+                "separator": True,
+                "items": [
+                    {
+                        "title": "Periodic Tasks",
+                        "icon": "schedule",
+                        "link": "/admin/django_celery_beat/periodictask/",
+                    },
+                ],
+            },
+        ],
+    },
+}
